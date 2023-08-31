33aedd999b4876cc553e72da137c0aeda4c64326
Gabriel Estiven Borrero Silva <g.borreros@uniandes.edu.co>
2023-08-31 18:23:39 -0500

    Avance

--- Taller_1/indices_refraccion.py
+++ Taller_1/indices_refraccion.py
@@ -1,9 +1,13 @@
 def tuplas_archivos_yml(ruta_de_archivo):
     with open(ruta_de_archivo, 'r') as archivo:
         contenido = archivo.readlines()
-   
-        
-    return contenido
+        list=[]
+        for i in contenido:
+            if i != float:
+                
+                list.append(i)
+            
+    return list.split("data: | -")
 
 print(tuplas_archivos_yml('C:\\Users\\gaboe\\Desktop\\FISI2526-MetCompCompl-202320\\Taller_1\\Plásticos_Comerciales\\French.yml'))
 print(tuplas_archivos_yml)
